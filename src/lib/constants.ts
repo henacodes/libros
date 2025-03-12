bdbc9b32ed46b3a0d69c1e3da26e8c439cc67f98
Henok Ayenew <117871961+henacodes@users.noreply.github.com>
2025-03-12 06:05:42 +0300

    changed API server to koyeb

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const API_SERVER_URL = 'https://libros-server.onrender.com'; //'http://localhost:3000'
+export const API_SERVER_URL = 'https://striking-deina-henacodes-4eadc4f0.koyeb.app'; //'http://localhost:3000'
 export const NOT_AVAILABLE = 'N/A';
 export const DOWNLOAD_HISTORY = 'download-history';
